7415251dc7fd0d5587124066e12fd1f4ac993b33
mohamed-044 <mohamed.boulatika04@gmail.com>
2024-05-14 07:02:01 +0200

    communication server client avec alert + boutons allumer/eteindre

--- GestionAppareils.js
+++ GestionAppareils.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { View, Text, FlatList, TouchableOpacity, Button } from 'react-native';
 import axios from 'axios';
-
+import { Alert } from 'react-native';
 
 // Données statiques pour les appareils
 const appareils = ['Appareil 1', 'Appareil 2', 'Appareil 3', 'Appareil 4', 'Appareil 5'];
 
 const GestionAppareils = () => {
-  // Fonction pour allumer l'appareil sélectionné
 // Fonction pour allumer l'appareil sélectionné
 const handleAllumer = (appareil) => {
-  // Send a POST request to the server with the message
+  console.log(`Appareil ${appareil} allumé`);
+  // Display an alert on the application
+  Alert.alert(`L'appareil n°${appareils.indexOf(appareil) + 1} est allumé`);
+  // Send a POST request to the server
   axios.post('http://192.168.0.20:3006/action', `L'appareil n°${appareils.indexOf(appareil) + 1} est allumé`)
     .then(response => {
       console.log('Response from server:', response.data);
-      // Display an alert on the application
-      Alert.alert(`L'appareil n°${appareils.indexOf(appareil) + 1} est allumé`, response.data);
     })
     .catch(error => {
       console.error('Error:', error.message);
@@ -24,21 +24,22 @@ const handleAllumer = (appareil) => {
     });
 };
 
- // Fonction pour éteindre l'appareil sélectionné
- const handleEteindre = (appareil) => {
-   // Send a POST request to the server with the message
-   axios.post('http://192.168.0.20:3006/action', `L'appareil n°${appareils.indexOf(appareil) + 1} est éteint`)
-     .then(response => {
-       console.log('Response from server:', response.data);
-       // Display an alert on the application
-       Alert.alert(`L'appareil n°${appareils.indexOf(appareil) + 1} est éteint`, response.data);
-     })
-     .catch(error => {
-       console.error('Error:', error.message);
-       // Display an error alert on the application
-       Alert.alert('Error', error.message);
-     });
- };
+// Fonction pour éteindre l'appareil sélectionné
+const handleEteindre = (appareil) => {
+  console.log(`Appareil ${appareil} éteint`);
+  // Display an alert on the application
+  Alert.alert(`L'appareil n°${appareils.indexOf(appareil) + 1} est éteint`);
+  // Send a POST request to the server
+  axios.post('http://192.168.0.20:3006/action', `L'appareil n°${appareils.indexOf(appareil) + 1} est éteint`)
+    .then(response => {
+      console.log('Response from server:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+      // Display an error alert on the application
+      Alert.alert('Error', error.message);
+    });
+};
 
   // Fonction pour rendre chaque élément de la liste des appareils
   const renderAppareilItem = ({ item }) => (
